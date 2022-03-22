6d2019deec887a4f47f5bde67f37ae3fafbb72bc
quynguyen <quy.nguyen@gmail.com>
2022-03-22 13:08:22 -0700

    Explicitly removed the gitinoring of .lua files in this directory, so that my config changes persist in git.  I'll only do so in this 'quy-user-config' branch though;  I'll keep the 'main' branch as is.

---
+++ lua/vapour/user-config/init.lua
@@ -0,0 +1,246 @@
+-- Vapour Settings
+Vapour.plugins.packer.init.max_jobs = 60
+Vapour.settings.colorscheme = 'dracula'
+Vapour.settings.lualine_colorscheme = 'dracula'
+Vapour.settings.transparent_bg = true
+Vapour.settings.always_force_write = true
+
+-- Vapour plugins
+Vapour.plugins.telescope.enabled = true
+Vapour.plugins.dashboard.enabled = true
+Vapour.plugins.toggleterm.enabled = true
+Vapour.plugins.treesitter.enabled = true
+Vapour.plugins.autopairs.enabled = true
+Vapour.plugins.lsp.enabled = true
+Vapour.plugins.stabline.enabled = true
+Vapour.plugins.staline.enabled = true
+Vapour.plugins.gitsigns.enabled = true
+Vapour.plugins.colorizer.enabled = true
+Vapour.plugins.null_ls.enabled = false -- Overriding the configuration that comes with Vapour
+
+
+-- Vapour.plugins.treesitter = {
+--   enabled = true,
+--   ensure_installed = "all",
+--   ignore_install = {},
+--   indent = {enable = false},
+--   highlight = {enable = true},
+--   autotag = {enable = true},
+--   endwise = {enable = true},
+--   rainbow = {enable = true, extended_mode = false, disable = {"html"}},
+--   textsubjects = {
+--     enable = true,
+--     prev_selection = ',', -- (Optional) keymap to select the previous selection
+--     keymaps = {
+--       ['.'] = 'textsubjects-smart',
+--       [';'] = 'textsubjects-container-outer',
+--       ['i;'] = 'textsubjects-container-inner',
+--     },
+--   },
+-- }
+
+-- Vapour Keybindgs -- via "Which-key"
+Vapour.plugins.which_key.user_defined.r = {
+  name = "Run",
+  e = {":!ruby %<CR>", "Execute Ruby"},
+  b = {":!bundle exec rspec %<CR>", "Bundle exec"},
+  r = {":!rspec . --color --format doc<CR>", "Rspec"},
+  f = {":Format<CR>", "Format"},
+  i = {"m'gg=G''", "Indent"}
+}
+Vapour.plugins.which_key.user_defined.Z = {":source $MYVIMRC<CR>", "Reload Neovim Config"}
+Vapour.plugins.which_key.user_defined.D = {
+  name = "DAP Debugger",
+  b = {":lua require'dap'.toggle_breakpoint()<CR>", "Toggle Breakpoint"}
+}
+
+-- User Plugins
+Vapour.plugins.user = {
+  -- Theme
+  {'Mofiqul/dracula.nvim'}, --
+  -- 
+  -- Classic File Tree
+  {'preservim/nerdtree'}, --
+  --
+  -- To swap two textobjects, target1 and target2, use 'cx' on target1 and 'cx' again (or '.') on target2
+  --
+  {'tommcdo/vim-exchange'},--
+  --
+  -- Yank Highlight
+  {'machakann/vim-highlightedyank'}, --
+  --
+  -- LSP Pop Info
+  {'glepnir/lspsaga.nvim'}, --
+  --
+  -- Ruby stuff
+  {'vim-ruby/vim-ruby'}, {'tpope/vim-rails'}, {'tpope/vim-bundler'}, {'tpope/vim-rake'},--
+  --
+  -- Tim Pop classicj
+  {'tpope/vim-repeat'}, {'tpope/vim-surround'}, {'tpope/vim-fugitive'}, {'tpope/vim-unimpaired'},
+  {'tpope/vim-projectionist'}, --
+  --
+  -- Tmux.conf syntax
+  {'tmux-plugins/vim-tmux'}, --
+  --
+  -- CamelCase and snake_case word jumping
+  {'bkad/CamelCaseMotion'}, --
+  -- IntelliJ-style Expand
+  {'terryma/vim-expand-region'}, --
+  --
+  -- Tags
+  {'ludovicchabant/vim-gutentags'}, --
+  --
+  -- Useful plugsin from a Rubyist
+  {'andrewradev/splitjoin.vim'}, {'AndrewRadev/sideways.vim'}, {'AndrewRadev/switch.vim'},
+  {'AndrewRadev/tagalong.vim'}, -- changing the name of <start> tag, also changes its </start> tag
+  --
+  -- Collection of Snippets
+  {'rafamadriz/friendly-snippets'}, --
+  --
+  -- Text custom_text_objects
+  {'kana/vim-textobj-user'},
+  {'kana/vim-textobj-line'}, {'kana/vim-textobj-entire'},
+  {'kana/vim-textobj-function'}, {'kana/vim-textobj-indent'},
+  {'vim-scripts/argtextobj.vim'}, --
+  {'thinca/vim-textobj-comment'}, {'thinca/vim-textobj-function-javascript'},
+  {'rhysd/vim-textobj-ruby'}, {'nelstrom/vim-textobj-rubyblock', requires = {'adelarsq/vim-matchit'}},
+  {'whatyouhide/vim-textobj-erb'}, --
+  {'coachshea/vim-textobj-markdown'}, --
+  --
+  -- Debugger
+  {"rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"}}
+}
+
+-- Quy's custom null-ls config (used for formatting/beautifying code)
+-- ** Disabled by Quy ** I need to tighten-up / generalize how different languages get cormatted, and the rules for their formatting.
+-- require('vapour.user-config.null-ls-config')
+
+-- Quy's custom LSP config
+require('vapour.user-config.lsp-config')
+
+-- augroup packer_user_config
+-- autocmd!
+-- autocmd BufWritePost init.lua source <afile> | PackerSync 
+-- augroup end
+vim.cmd([[
+
+
+map <silent> w <Plug>CamelCaseMotion_w
+map <silent> b <Plug>CamelCaseMotion_b
+map <silent> e <Plug>CamelCaseMotion_e
+map <silent> ge <Plug>CamelCaseMotion_ge
+sunmap w
+sunmap b
+sunmap e
+sunmap ge
+
+noremap * :set hlsearch<CR>:nohlsearch<CR>*
+noremap / :set hlsearch<CR>:nohlsearch<CR>/
+noremap ? :set hlsearch<CR>:nohlsearch<CR>?
+nnoremap <F4> :set invhlsearch<CR>
+inoremap <F4> <ESC>:set invhlsearch<CR>gi
+nnoremap <CR> :set nohlsearch<CR>
+
+nnoremap <S-H> :SidewaysLeft<cr>
+nnoremap <S-L> :SidewaysRight<cr>
+
+
+" Top 5 vimrc remaps from this crazy mofo
+" https://www.youtube.coim/watch?v=hSHATqh8svM
+"================================================================================================ 
+" Capital "D", and Capital "C" effects from the cursor to the end of the line.
+" Yet, Capital "Y" yanks the entire line.  Why?  Make "Y" yank from the cursor
+" until the end of the line
+nnoremap Y y$
+
+" When the cursor jumps during finds or joins, have the screen/camera track
+" the cursor, keeping the cursor in the middle.  This is a like the David
+" Fincher camera think, or keeping the subject centered in frame, even if the
+" subject is moving ever so slightly
+nnoremap n nzzzv
+nnoremap N Nzzzv
+nnoremap J mzJ`z
+
+" This is a helpful ditty to undo just a phrase, or sentence, rather than a whole line.
+" Create undo-breakss for a comma, period, exclamation, or question mark; whatever really.
+inoremap , ,<c-g>u
+inoremap . .<c-g>u
+inoremap ! !<c-g>u
+inoremap ? ?<c-g>u
+
+noremap <expr> k (v:count > 5 ? "m'" . v:count : "") . 'k'
+nnoremap <expr> j (v:count > 5 ? "m'" . v:count : "") . 'j'
+
+"================================================================================================
+" Required for Ruby-block 
+" https://github.com/nelstrom/vim-textobj-rubyblock 
+"================================================================================================
+runtime macros/matchit.vim
+
+"================================================================================================
+" Enable framework snippets
+"================================================================================================
+let g:vsnip_filetypes = {}
+let g:vsnip_filetypes.ruby = ['rails']
+
+"================================================================================================
+" vim-expand.git settings
+" https://github.com/terryma/vim-expand-region/ 
+"================================================================================================
+
+"================================================================================================
+" Misc stuff that I should propbably delate"
+"================================================================================================
+
+set t_ZH=^[[3m
+set t_ZR=^[[23m
+
+let &t_8f="\<Esc>[38;2;%lu;%lu;%lum"
+let &t_8b="\<Esc>[48;2;%lu;%lu;%lum"
+
+highlight Comment cterm=italic
+
+set termguicolors
+]])
+
+-- vim.cmd[[
+-- let g:expand_region_text_objects = {
+--   \ 'iw'  :0,
+--   \ 'iW'  :0,
+--   \ 'i"'  :0,
+--   \ 'i''' :0,
+--   \ 'i]'  :1,
+--   \ 'ib'  :1,
+--   \ 'iB'  :1,
+--   \ 'il'  :0,
+--   \ 'ip'  :0,
+--   \ 'ie'  :0,
+--   \ }
+-- 
+-- call expand_region#custom_text_objects({
+--   \ "\/\\n\\n\<CR>": 1,
+--   \ 'a]' :1,
+--   \ 'ab' :1,
+--   \ 'aB' :1,
+--   \ 'i>' :1,
+--   \ 'a>' :1,
+--   \ 'it' :1,
+--   \ 'at' :1,
+--   \ 'ic' :0,
+--   \ 'ac' :0,
+--   \ 'ii' :0,
+--   \ 'ai' :0,
+--   \ 'if' :0,
+--   \ 'af' :0,
+--   \ 'im' :0,
+--   \ 'am' :0,
+--   \ 'ir' :0,
+--   \ 'ar' :0,
+--   \ })
+-- 
+-- let g:textobj_ruby_more_mappings = 1
+-- let g:expand_region_text_objects_ruby = {
+--   \ 'im' :0, 
+--   \ 'am' :0, 
+--   \ }
+-- ]]
---
+++ lua/vapour/user-config/lsp-config/init.lua
@@ -0,0 +1,183 @@
+--------------------------------------------------------------------------------
+-- Note to QUY.  You're not using this file yet, as you don't seem to need it.
+--------------------------------------------------------------------------------
+--   if client.resolved_capabilities.document_highlight then
+--     vim.api.nvim_exec([[
+--   augroup lsp_document_highlight
+--     autocmd! * <buffer>
+--     autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+--     autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+--   augroup END
+-- ]], false)
+--   end
+Vapour.plugins.lsp.enabled = true
+local lsp_installer = Vapour.utils.plugins.require('nvim-lsp-installer')
+
+-- "sumneko_lua", -- LUA
+-- "remark_ls", -- Markdown
+-- "html", -- HTML
+-- "cssls", -- CSS
+-- "quick_lint_js", -- Javscript
+-- "jsonls", -- JSON
+-- "tsserver", -- Typescript
+-- "tailwindcss", -- Tailwind
+-- "vuels", -- Vue
+-- "svelte", -- Svelte
+-- "graphql", -- GraphQL
+-- "solargraph", -- Ruby
+-- "sorbet", -- Type-checked Ruby
+-- "pyright", -- Python
+-- "sorbet", -- Type-checked Ruby
+-- "kotlin_language_server", --Kotlin
+-- "jdtls", -- Java
+-- "sourcekit", -- Swift
+-- "rust_analyzer", -- Rust
+-- "gols", -- Go
+-- "sqlls", -- SQL
+-- "yamlls", -- Yaml
+-- "lemminx", -- XML
+-- "bashls", -- Bash
+
+-- Include the servers you want to have installed by default below
+local servers = {
+  "sumneko_lua", -- LUA
+  "remark_ls", -- Markdown
+  "html", -- HTML
+  "emmet_ls", -- Emmet
+  "cssls", -- CSS
+  "quick_lint_js", -- Javscript
+  "jsonls", -- JSON
+  "tsserver", -- Typescript
+  "graphql", -- GraphQL
+  "solargraph", -- Ruby
+  "sorbet", -- Type-checked Ruby
+  "pyright", -- Python
+  "sorbet", -- Type-checked Ruby
+  "sqlls", -- SQL
+  "yamlls", -- Yaml
+  "lemminx", -- XML
+  "bashls" -- Bash
+}
+
+-- i = {":LspInfo<cr>", "Connected Language Servers"},
+-- k = {'<cmd>lua vim.lsp.buf.signature_help()<CR>', "Signature help"},
+-- K = {'<cmd>lua vim.lsp.buf.hover()<CR>', "Hover"},
+-- w = {'<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', "Add workspace folder"},
+-- W = {'<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', "Remove workspace folder"},
+-- l = {
+--   '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+--   "List workspace folder"
+-- },
+-- t = {'<cmd>lua vim.lsp.buf.type_definition()<CR>', "Type definition"},
+-- d = {'<cmd>lua vim.lsp.buf.definition()<CR>', "Go to definition"},
+-- D = {'<cmd>lua vim.lsp.buf.delaration()<CR>', "Go to declaration"},
+-- r = {'<cmd>lua vim.lsp.buf.references()<CR>', "References"},
+-- R = {'<cmd>lua vim.lsp.buf.rename()<CR>', "Rename"},
+-- a = {'<cmd>lua vim.lsp.buf.code_action()<CR>', "Code actions"},
+-- e = {'<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', "Show line diagnostics"},
+-- n = {'<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', "Go to next diagnostic"},
+-- N = {'<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', "Go to previous diagnostic"},
+-- I = {'<cmd>LspInstallInfo<cr>', 'Install language server'}
+
+local on_attach = function(client, bufnr)
+  local opts = {noremap = true, silent = false}
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gK', '<cmd>:Lspsaga signature_help<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gK', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gk', '<cmd>:Lspsaga hover_doc<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gk', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gR', '<cmd>:Lspsaga rename<CR>', opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gR', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '[e', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']e', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gE', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gC', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa',
+                              '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr',
+                              '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl',
+                              '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+                              opts)
+  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+
+  if client.resolved_capabilities.document_highlight then
+    vim.api.nvim_exec([[
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]], false)
+  end
+end
+
+local debounce_time = 1000
+
+for _, lsp_name in pairs(servers) do
+  local server_is_found, server = lsp_installer.get_server(lsp_name)
+  if server_is_found and not server:is_installed() then server:install() end
+
+  -- Configure
+  if server_is_found then
+    if lsp_name == 'sumneko_lua' then
+      Vapour.language_servers['sumneko_lua'] = {
+        config = function(opts)
+          opts = vim.tbl_deep_extend("force", {
+            on_attach = on_attach,
+            flags = {debounce_text_changes = debounce_time},
+            settings = {
+              Lua = {
+                runtime = {version = 'LuaJIT', path = vim.split(package.path, ';')},
+                diagnostics = {globals = {'vim'}},
+                workspace = {
+                  library = vim.api.nvim_get_runtime_file("", true),
+                  checkThirdParty = false,
+                  preloadFileSize = 10000
+                },
+                telemetry = {enable = false}
+              }
+            }
+          }, opts)
+          return opts
+        end
+      }
+    elseif lsp_name == 'solargraph' then
+      Vapour.language_servers[lsp_name] = {
+        config = function(opts)
+          opts = vim.tbl_deep_extend("force", {
+            on_attach = on_attach,
+            flags = {debounce_text_changes = debounce_time},
+            settings = {solargraph = {diagnostics = true}}
+          }, opts)
+          return opts
+        end
+      }
+    elseif lsp_name == 'emmet_ls' then
+      Vapour.language_servers[lsp_name] = {
+        config = function(opts)
+          opts = vim.tbl_deep_extend("force", {
+            on_attach = on_attach,
+            flags = {debounce_text_changes = debounce_time},
+            filetypes = { "eruby", "erb", "html", "css", "typescriptreact", "javascriptreact" },
+          }, opts)
+          return opts
+        end
+      }
+    else
+      Vapour.language_servers[lsp_name] = {
+        config = function(opts)
+          opts = vim.tbl_deep_extend("force", {
+            on_attach = on_attach,
+            flags = {debounce_text_changes = debounce_time}
+          }, opts)
+          return opts
+        end
+      }
+    end
+  end
+end
